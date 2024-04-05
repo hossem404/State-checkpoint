a3e6b09c673abd27f1c343e101bfce0f2e360ea4
hossem404 <rafbeach@gmail.com>
2024-04-05 21:05:12 +0100

    first committt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props)  {
+    super(props)
+    this.state={
+    person: {
+      fullName: 'Saïd Kasmi',
+      bio: 'Né en 1976, Saïd Kasmi-Mitterrand est l un des trois fils de Frédéric Mitterrand, neveu de l ancien président de la République François Mitterrand.',
+      imgSrc: ' https://static1.purepeople.com/articles/0/37/34/60/@/5391497-said-kasmi-mitterrand-lors-de-la-ceremon-1200x0-2.jpg',
+      profession: 'producteur et réalisateur'
+    },
+    shows: true,
+    mountedTime:null
+  }
+}
+  click =()=>{
+    this.setState({shows:!this.state.shows})
+  }
+  componentDidMount() {
+    this.setState({ mountedTime: new Date() });
+    this.interval = setInterval(() => {
+      console.log('ok')
+    }, 5000);
+  }
+  
+  render() {
+    
+    return (
+      <div>
+        <button onClick={this.click}>click here !</button>
+       {this.state.shows===true && 
+       <h3>Name:{this.state.person.fullName}</h3>
+       }
+       {this.state.shows===true && 
+       <h3>Name:{this.state.person.bio}</h3>
+       }
+       {this.state.shows===true && 
+       <h3>Name:{this.state.person.profession}</h3>
+       }
+       {this.state.shows===true && 
+       <img src={this.state.person.imgSrc} style={{ width: '200px', height: 'auto' }} />
+       }
+      {/* <p>{Math.floor((new Date() - this.state.mountedTimemountedTime) / 1000)} seconds</p> */}
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+export default App
